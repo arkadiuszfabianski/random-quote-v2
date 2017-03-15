5e09ece202da7469d07c1ae46f6426b23047d21b
arkadiuszfabianski <arkadiusz.fabianski@gmail.com>
2017-03-15 19:57:10 +0100

    2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+var randomNumber;
+var quoteObject;
+var printedText;
 var quotes = [
     {quote: "The coldest  winter I ever spent was a summer in San Fransisco.", source: "Mark Twain"},
     {quote: "You can do anyting, but not everything.", source: "Woody Allen"},
@@ -10,15 +12,13 @@ var quotes = [
 ];
 
 function getRandomQuote(){
-    var randomNumber = Math.floor(Math.random()*4); //selects a random quote object from the quotes array
-    console.log("Random Number is:" + randomNumber);
+    randomNumber = Math.floor(Math.random()*quotes.length); //selects a random quote object from the quotes array
     return quotes[randomNumber];//returns the randomly selected quote object
 };
 
 function printQuote(){
-    var quoteObject = getRandomQuote(); //calls the getRandomQuote function and stores the returned quote object in a variable
-    console.log("Quote: " +quoteObject.quote + ", Author: " + quoteObject.source);
-    var printedText = "<p class=\"quote\">" + quoteObject.quote + "</p><p class=\"source\"> " + quoteObject.source + "</p>"; //html text
-    console.log(printedText);
+    quoteObject = getRandomQuote(); //calls the getRandomQuote function and stores the returned quote object in a variable
+    printedText = "<p class=\"quote\">" + quoteObject.quote + "</p><p class=\"source\"> " + quoteObject.source + "</p>"; //html text
     document.getElementById('quote-box').innerHTML = printedText; //print onto page
 };
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
